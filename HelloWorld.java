e81e9ef3d4656ef1bc7c6067d3f0d261f339fa35
EarlErrmeen2001 <u22swe2001@ccuk.edu.ng>
2024-10-22 14:18:38 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 // This is a simple Java program.
 // The FileName : "HelloWorld.java".
 
-class HelloWorld {
+public class HelloWorld {
     // The program begins with a call to main().
     // Aim is to print "Hello, World" to the terminal window.
     public static void main(String args[])
