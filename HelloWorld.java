aaddaa253b188a616fd94e606f110d3709b92019
EarlErrmeen2001 <u22swe2001@ccuk.edu.ng>
2024-10-22 13:51:09 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+// This is a simple Java program.
+// The FileName : "HelloWorld.java".
+
+class HelloWorld {
+    // The program begins with a call to main().
+    // Aim is to print "Hello, World" to the terminal window.
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World");
+    }
+}
